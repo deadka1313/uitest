5b23c0b3503ff41317ea4df9ccbf5e442e79d8a0
deadka1313 <deadka1313@gmail.com>
2018-02-08 03:30:25 +0700

    Исправил баг связанности нескольких слайдеров, добавил выбор начальльного положения слайдера

--- source/common.blocks/slider/slider.js
+++ source/common.blocks/slider/slider.js
@@ -1,30 +1,33 @@
-require('webpack-jquery-ui');
+require('webpack-jquery-ui/slider');
 
-$( function() {
-  var handle = $( "#slider_block-value" );
-  $( ".slider__step1" ).slider({
+$( ".slider__step1" ).each(function() {
+  var value = parseInt( $( this ).text(), 10 );
+  $( this ).slider({
+    value: value,
     classes: {
       "ui-slider": "slider_track",
       "ui-slider-handle": "slider_handle__step1"
     },
     create: function() {
-      handle.text( $( this ).slider( "value" ) );
+      $( "#"+$(this).attr('id')+" #slider_block-value" ).text( $( this ).slider( "value" ) );
     },
     slide: function( event, ui ) {
-      handle.text( ui.value );
+      $( "#"+$(this).attr('id')+" #slider_block-value" ).text( ui.value );
     }
   });   
 } );
 
-$( function() {
-  $( ".slider__step25" ).slider({
+$( ".slider__step25" ).each(function() {
+  var value = parseInt( $( this ).text(), 10 );
+  $( this ).slider({
     min: 0,
     max: 100,
     step: 25,
     range: "min",
+    value: value,
     classes: {
       "ui-slider": "slider_track",
       "ui-slider-handle": "slider_handle__step25"
     }
-  });   
+  });  
 } );
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,4 +1,13 @@
 import './index.styl';
 import 'normalize.css';
 
+require('webpack-jquery-ui/datepicker');
+
 import '../../common.blocks/slider/slider';
+
+$('#calendar').datepicker({
+  inline: true,
+  firstDay: 1,
+  showOtherMonths: true,
+  dayNamesMin: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+  });
