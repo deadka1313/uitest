ca0061e6efaec275aca582d7dfc7709579c387cf
deadka1313 <deadka1313@gmail.com>
2018-02-08 19:25:17 +0700

    Исправлено отображение ripple effect. Ранее неправильно расчитывались координаты при прокрутке страницы

--- source/common.blocks/button/button.js
+++ source/common.blocks/button/button.js
@@ -8,12 +8,12 @@ function createRipple (e) {
   var circle = document.createElement('div');
   this.appendChild(circle);
 
-  var d = Math.max(this.clientWidth, this.clientHeight);
+  var d = 30;
 
   circle.style.width = circle.style.height = d + 'px';
 
-  circle.style.left = e.clientX - this.offsetLeft - d / 2 + 'px';
-  circle.style.top = e.clientY - this.offsetTop  - d / 2 + 'px';
+  circle.style.left = e.pageX - this.offsetLeft - d / 2 + 'px';
+  circle.style.top = e.pageY - this.offsetTop  - d / 2 + 'px';
 
   circle.classList.add('ripple');
 }
