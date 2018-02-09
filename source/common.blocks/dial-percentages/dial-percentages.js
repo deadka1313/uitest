33ffc7b6c51eb099604c647a0c5bda9ff6e6a27b
deadka1313 <deadka1313@gmail.com>
2018-02-09 14:21:38 +0700

    Доделал круговую процентную диаграмму(добавил текс в центр)

--- source/common.blocks/dial-percentages/dial-percentages.js
+++ source/common.blocks/dial-percentages/dial-percentages.js
@@ -2,8 +2,8 @@ var Circle = function(sel){
 	var circles = document.querySelectorAll(sel);
 	[].forEach.call(circles,function(el){
 			var valEl = parseFloat(el.innerHTML);
-			valEl = valEl*282/100;
-			el.innerHTML = '<svg width="95" height="95"><circle transform="rotate(-90)" r="45" cx="-47.5" cy="47.5" /><circle transform="rotate(-90)" style="stroke-dasharray:'+valEl+'px 282px;" r="45" cx="-47.5" cy="47.5" /><text x="50%" y="50%">Text</text></svg>';
+			dashvalEl = valEl*282/100;
+			el.innerHTML = '<svg width="95" height="95"><circle transform="rotate(-90)" r="45" cx="-47.5" cy="47.5" /><circle transform="rotate(-90)" style="stroke-dasharray:'+dashvalEl+'px 282px;" r="45" cx="-47.5" cy="47.5" /></svg>';
 			
 	});
 };
