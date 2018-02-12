352aedc111c4a3f53a9f7a3cb834a2828701fa3c
deadka1313 <deadka1313@gmail.com>
2018-02-12 14:34:08 +0700

    form mixin

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -6,6 +6,7 @@ require('webpack-jquery-ui/datepicker');
 import '../../common.blocks/slider/slider';
 import '../../common.blocks/button/button';
 import '../../common.blocks/dial-percentages/dial-percentages';
+import '../../common.blocks/form/form';
 
 $('#calendar').datepicker({
   inline: true,
